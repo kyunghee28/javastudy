e2080738c463fddcbdef33aaaa3bd73f6960deb6
kyunghee28 <pori4339@naver.com>
2022-05-23 11:03:16 +0900

    객체지향 프로그래밍 - 오버로딩

--- src/ch06/MyMathTest2.java
+++ src/ch06/MyMathTest2.java
@@ -30,7 +30,7 @@ class MyMath2{
     long multiply() {return a * b; }
     double divide() {return a / b; }
 
-    // 인스턴스변수와 관계없이 매개변수만으로 작업이 가능하다.
+    // 인스턴스변수와 관계없이 매개변수만으로 작업이 가능하므로 static 을 붙여서 클래스 메서드로 선언
     static long add(long a, long b) {return a + b; }    // a, b 는 지역변수
     static long substract(long a, long b) {return a - b; }
     static long multiply(long a, long b) {return a * b; }
--- src/ch06/OverloadingTest.java
+++ src/ch06/OverloadingTest.java
@@ -5,7 +5,12 @@ public class OverloadingTest {
     public static void main(String [] args) {
         MyMath3 mm = new MyMath3();
         System.out.println("mm.add(3, 3) 결과 : " + mm.add(3,3));
+        System.out.println("mm.add(3L, 3) 결과 : " + mm.add(3L,3));
+        System.out.println("mm.add(3, 3L) 결과 : " + mm.add(3,3L));
+        System.out.println("mm.add(3L, 3L) 결과 : " + mm.add(3L,3L));
 
+        int[] a = {100, 200, 300};
+        System.out.println("mm.add(a) 결과 : " + mm.add(a));
     }
 
 }
@@ -16,9 +21,38 @@ class MyMath3 {
         return a+b;
     }
 
+    long add(int a, long b) {
+        System.out.print("int long(int a, long b) - ");
+        return a+b;
+    }
+
+    long add(long a, int b) {
+        System.out.print("int long(long a, int b) - ");
+        return a+b;
+    }
+
+    long add(long a, long b) {
+        System.out.print("int long(long a, long b) - ");
+        return a+b;
+    }
+
+    // 베열의 모든 요소의 합을 결과로 돌려준다.
+    int add(int[] a) {
+        System.out.print("int long(int[] a) - ");
+        int result = 0;
+        for(int i = 0 ; i < a.length ; i++){
+            result += a[i];
+        }
+        return result;
+    }
+
+
 }
 
 /** 실행결과
  *  int add(int a, int b) - mm.add(3, 3) 결과 : 6
- *
+ *  int long(long a, int b) - mm.add(3L, 3) 결과 : 6
+ *  int long(int a, long b) - mm.add(3, 3L) 결과 : 6
+ *  int long(long a, long b) - mm.add(3L, 3L) 결과 : 6
+ *  int long(int[] a) - mm.add(a) 결과 : 600
  * */
